82cfef28cb563db0e47c82686bdee7715206f777
beolafsen <76835718+beolafsen@users.noreply.github.com>
2022-01-11 08:29:32 +0100

    Add project files.

---
+++ TestAutomapperAndStripe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Stripe;
+using AutoMapper;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<VMStripeInvoiceItem, Stripe.InvoiceItem>();
+                c.CreateMap<Stripe.InvoiceItem, VMStripeInvoiceItem>()
+                .ForMember(pts => pts.LineAmount, opt => opt.MapFrom(ps => ps.Quantity * (ps.UnitAmountDecimal ?? (decimal)0) * (decimal)0.01))
+                .ForMember(pts => pts.UnitAmountDecimal, opt => opt.MapFrom(ps => (ps.UnitAmountDecimal ?? (decimal)0) * (decimal)0.01));
+            });
+            
+            config.AssertConfigurationIsValid();
+
+            var mapper = config.CreateMapper();
+
+            Console.WriteLine("test");
+        }
+    }
+
+    public class VMStripeInvoiceItem : InvoiceItem
+    {
+        public decimal LineAmount { get; set; }
+    }
+}
